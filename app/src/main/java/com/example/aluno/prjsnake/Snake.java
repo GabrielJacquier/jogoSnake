d58763df712bf416396e38e79282471e25467980
Gabriel Safado <gabriel.jacquier@tegra.me>
2017-04-29 16:44:24 -0300

    Melhora regras do jogo

--- app/src/main/java/com/example/aluno/prjsnake/Snake.java
+++ app/src/main/java/com/example/aluno/prjsnake/Snake.java
@@ -1,6 +1,7 @@
 package com.example.aluno.prjsnake;
 
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.provider.Contacts;
 
@@ -8,6 +9,7 @@ import android.provider.Contacts;
  * Created by aluno on 18/04/2017.
  */
 public class Snake {
+
     public static enum DirecoesEnum {
         DIREITA,
         ACIMA,
@@ -16,6 +18,7 @@ public class Snake {
         PARADO
     }
 
+    boolean perdeu = false;
     int x[] = new int[300];
     int y[] = new int[300];
     int tam = 3;
@@ -25,11 +28,11 @@ public class Snake {
     private DirecoesEnum direcao = DirecoesEnum.PARADO;
 
     public Snake(int x, int y) {
-        this.x[0] = x;  // cabeça da cobra em x
+        this.x[0] = x + 100;  // cabeça da cobra em x
         this.y[0] = y;  // cabeça da cobra em y
         this.x[1] = x + 50;
         this.y[1] = y;
-        this.x[2] = x + 100;
+        this.x[2] = x;
         this.y[2] = y;
         tam = 3;
     }
@@ -48,22 +51,22 @@ public class Snake {
         }
     }
 
-    public void moverDireita() {
+    private void moverDireita() {
         moverCorpo();
         x[0] = x[0] + 50;
     }
 
-    public void moverEsquerda() {
+    private void moverEsquerda() {
         moverCorpo();
         x[0] = x[0] - 50;
     }
 
-    public void moverBaixo() {
+    private void moverBaixo() {
         moverCorpo();
         y[0] = y[0] + 50;
     }
 
-    public void moverCima() {
+    private void moverCima() {
         moverCorpo();
         y[0] = y[0] - 50;
     }
@@ -73,24 +76,45 @@ public class Snake {
     }
 
     public void moverSnake() {
-        switch (direcao) {
-            case DIREITA:
-                if((x[0] + 50) <= widthScreen)
-                    moverDireita();
-                break;
-            case ESQUERDA:
-                if((x[0] - 50) >= 0)
-                    moverEsquerda();
-                break;
-            case ACIMA:
-                if((y[0] - 50) >= 0)
-                    moverCima();
-                break;
-            case BAIXO:
-                if((y[0] - 50) <= (heightScreen - 450))
-                    moverBaixo();
-                break;
+        if(!perdeu) {
+            switch (direcao) {
+                case DIREITA:
+                    if ((x[0] + 50) <= widthScreen)
+                        moverDireita();
+                    break;
+                case ESQUERDA:
+                    if ((x[0] - 50) >= 0)
+                        moverEsquerda();
+                    break;
+                case ACIMA:
+                    if ((y[0] - 50) >= 0)
+                        moverCima();
+                    break;
+                case BAIXO:
+                    if ((y[0] + 50) <= (heightScreen - 310))
+                        moverBaixo();
+                    break;
+            }
         }
+        perdeu = cabecaBateuNoCorpo(x[0], y[0]);
+    }
+
+    public boolean comeuPonto(Rect ponto) {
+        if(ponto.contains(x[0], y[0])) {
+            this.tam += 1;
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean cabecaBateuNoCorpo(int x, int y) {
+        for (int i = tam - 1; i > 0 ; i--) {
+            if(this.x[i] == x && this.y[i] == y) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void setWidthScreen(int widthScreen) {
@@ -99,6 +123,19 @@ public class Snake {
 
     public void setHeightScreen(int heightScreen) {
         this.heightScreen = heightScreen;
+    }
+
+    public boolean posicaoPertenceASnake(int x, int y) {
+        for (int i = tam - 1; i >= 0 ; i--) {
+            if(this.x[i] == x && this.y[i] == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPerdeu() {
+        return perdeu;
     }
 
 }
--- app/src/main/java/com/example/aluno/prjsnake/Tela.java
+++ app/src/main/java/com/example/aluno/prjsnake/Tela.java
@@ -7,12 +7,18 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by aluno on 18/04/2017.
  */
 public class Tela extends View {
-    private Paint paint;
+
+    private Paint paintRed;
+    private Paint paintBlack;
     private Drawable imgFundo;
     private Drawable imgQuadrado;
     private int width, height;
@@ -21,31 +27,33 @@ public class Tela extends View {
     private Rect btnEsquerda;
     private Rect btnBaixo;
     private Rect btnCima;
+    private Rect ponto = new Rect(350, 350, 400, 400);;
 
     public Tela(Context context) {
         super(context);
-        imgFundo = context.getResources().getDrawable(R.drawable.koala);
         imgQuadrado = context.getResources().getDrawable(R.drawable.quadrado);
-        paint = new Paint();
-        paint.setARGB(255, 0, 0, 255);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(5);
+        paintRed = new Paint();
+        paintRed.setARGB(255, 0, 0, 255);
+        paintRed.setStyle(Paint.Style.FILL);
+
+        paintBlack = new Paint();
+        paintBlack.setARGB(255, 0, 0, 0);
+        paintBlack.setStyle(Paint.Style.FILL);
         cobra = new Snake(150, 150);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        imgFundo.setBounds(0, 0, this.width, this.height);
-        imgFundo.draw(canvas);
         btnBaixo = new Rect(width - 300, height - 100, width - 200, height);
         btnDireita = new Rect(width - 200, height - 200, width - 100, height - 100);
         btnEsquerda = new Rect(width - 400, height - 200, width - 300, height - 100);
         btnCima = new Rect(width - 300, height - 300, width - 200, height - 200);
-        canvas.drawRect(btnDireita, paint);
-        canvas.drawRect(btnEsquerda, paint);
-        canvas.drawRect(btnBaixo, paint);
-        canvas.drawRect(btnCima, paint);
+        canvas.drawRect(btnDireita, paintRed);
+        canvas.drawRect(btnEsquerda, paintRed);
+        canvas.drawRect(btnBaixo, paintRed);
+        canvas.drawRect(btnCima, paintRed);
+        canvas.drawRect(ponto, paintBlack);
         cobra.draw(imgQuadrado, canvas);
         invalidate();
     }
@@ -63,27 +71,54 @@ public class Tela extends View {
         float xt = event.getX();
         float yt = event.getY();
         int acao = event.getAction();
-        if(acao == MotionEvent.ACTION_DOWN) {
-            if(btnDireita.contains((int) xt, (int) yt)) {
+        if (acao == MotionEvent.ACTION_DOWN) {
+            if (btnDireita.contains((int) xt, (int) yt)) {
                 cobra.setDirecao(Snake.DirecoesEnum.DIREITA);
                 cobra.moverSnake();
             }
 
-            if(btnEsquerda.contains((int) xt, (int) yt)) {
+            if (btnEsquerda.contains((int) xt, (int) yt)) {
                 cobra.setDirecao(Snake.DirecoesEnum.ESQUERDA);
                 cobra.moverSnake();
             }
 
-            if(btnBaixo.contains((int) xt, (int) yt)) {
+            if (btnBaixo.contains((int) xt, (int) yt)) {
                 cobra.setDirecao(Snake.DirecoesEnum.BAIXO);
                 cobra.moverSnake();
             }
 
-            if(btnCima.contains((int) xt, (int) yt)) {
+            if (btnCima.contains((int) xt, (int) yt)) {
                 cobra.setDirecao(Snake.DirecoesEnum.ACIMA);
                 cobra.moverSnake();
+            }
+            if(cobra.comeuPonto(ponto)) {
+                int posicaoWidth;
+                int posicaoHeight;
+                do {
+                    int qtdPontosWidth = width / 50;
+                    int qtdPontosHeight = height / 50;
+                    Random randomGenerator = new Random();
+                    posicaoWidth = randomGenerator.nextInt(qtdPontosWidth - 1) * 50;
+                    posicaoHeight = randomGenerator.nextInt(qtdPontosHeight - 1) * 50;
+                } while(cobra.posicaoPertenceASnake(posicaoWidth - 50, posicaoHeight - 50)
+                        || novaPosicaoPontoInvalida(posicaoWidth, posicaoHeight));
+                ponto.set(posicaoWidth - 50, posicaoHeight - 50, posicaoWidth, posicaoHeight);
+            }
+
+            if(cobra.isPerdeu()) {
+                Toast.makeText(this.getContext(), "Você perdeu!", Toast.LENGTH_LONG).show();
             }
         }
         return true;
     }
+
+    private boolean novaPosicaoPontoInvalida(int x, int y) {
+        if((this.height - 310) < y) {
+            return true;
+        }
+        if(x <= 0 || y <= 0) {
+            return true;
+        }
+        return false;
+    };
 }
